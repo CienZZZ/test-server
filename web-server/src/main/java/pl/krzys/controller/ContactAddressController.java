cdcfea19a14040083ece71329f3ee1c4185e7dcf
CienZZZ <Krzysztofkan@poczta.fm>
2020-06-22 19:08:09 +0200

    write ContactAddressController

--- web-server/src/main/java/pl/krzys/controller/ContactAddressController.java
+++ web-server/src/main/java/pl/krzys/controller/ContactAddressController.java
@@ -1,77 +1,67 @@
 package pl.krzys.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import pl.krzys.model.ContactAddress;
-import pl.krzys.repository.ContactAddressRepository;
+import pl.krzys.dto.ContactAddressDTO;
+import pl.krzys.service.ContactAddressService;
 
-import java.util.ArrayList;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/contactaddress")
 public class ContactAddressController {
-    @Autowired
-    ContactAddressRepository repository;
-    
-    @GetMapping()
-    public ResponseEntity<List<ContactAddress>> getAll() {
-        try {
-            List<ContactAddress> list = new ArrayList<>();
 
-            repository.findAll().forEach(list::add);
-    
-            if(list.isEmpty())
-                    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            
-            return new ResponseEntity<>(list, HttpStatus.OK);
-    
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    private static final Logger log = LoggerFactory.getLogger(ContactAddressController.class);
+
+    ContactAddressService contactAddressService;
+
+    public ContactAddressController(ContactAddressService contactAddressService) {
+        this.contactAddressService = contactAddressService;
     }
-    
+
+    @GetMapping()
+    public ResponseEntity<List<ContactAddressDTO>> getAll() {
+        log.info("received request to list contact Addresses");
+        return ResponseEntity.ok(contactAddressService.getAllContactsAddresses().asJava());
+    }
+
     @GetMapping("/{id}")
-    public ResponseEntity<ContactAddress> getOneById(@PathVariable("id") long id)
+    public ResponseEntity<ContactAddressDTO> getOneById(@PathVariable("id") Long id)
     {
-        Optional<ContactAddress> data = repository.findById(id);
-        if(data.isPresent())
-            return new ResponseEntity<>(data.get(), HttpStatus.OK);
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        log.info("received request to get one contact address");
+        return ResponseEntity.ok(contactAddressService.getContactAddressById(id));
     }
+
 
     @PostMapping
-    public ResponseEntity<ContactAddress> create(@RequestBody ContactAddress data) {
-        try {
-            ContactAddress created = repository.save(data);
-            return new ResponseEntity<>(created, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
-        }
+    public ResponseEntity<ContactAddressDTO> create(@RequestBody ContactAddressDTO data) throws URISyntaxException {
+        log.info("received request to create contact address");
+        ContactAddressDTO createdContactAddress = contactAddressService.createNew(data);
+        return ResponseEntity.created(new URI("/api/contactaddress/" + data.getId())).body(createdContactAddress);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ContactAddress> updateOne(@PathVariable("id") long id, @RequestBody ContactAddress data)
+    public ResponseEntity<ContactAddressDTO> updateOne(@PathVariable("id") Long id, @RequestBody ContactAddressDTO data)
     {
-        Optional<ContactAddress> original = repository.findById(id);
-        if(!original.isPresent())
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        data.setId(id);
-        return new ResponseEntity<>(repository.save(data), HttpStatus.OK);
+        log.info("received request to update contact address");
+        ContactAddressDTO contactAddressToUpdate = contactAddressService.getContactAddressById(id);
+        if (contactAddressToUpdate != null) {
+            data.setId(id);
+            contactAddressService.update(data);
+        }
+        return ResponseEntity.ok(data);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<HttpStatus> deleteOne(@PathVariable("id") long id)
+    public ResponseEntity<HttpStatus> deleteOne(@PathVariable("id") Long id)
     {
-        try {
-            repository.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
-        }
+        log.info("received request to delete contact address");
+        contactAddressService.delete(id);
+        return ResponseEntity.noContent().build();
     }
-
 }
