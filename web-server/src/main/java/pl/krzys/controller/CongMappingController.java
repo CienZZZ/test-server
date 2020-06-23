7c2ae0115b154966d58e28b070c56323f8deb15b
CienZZZ <Krzysztofkan@poczta.fm>
2020-06-23 10:29:34 +0200

    write CongMappingController

--- web-server/src/main/java/pl/krzys/controller/CongMappingController.java
+++ web-server/src/main/java/pl/krzys/controller/CongMappingController.java
@@ -1,77 +1,67 @@
 package pl.krzys.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import pl.krzys.model.CongMapping;
-import pl.krzys.repository.CongMappingRepository;
+import pl.krzys.dto.CongMappingDTO;
+import pl.krzys.service.CongMappingService;
 
-import java.util.ArrayList;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/congmapping")
 public class CongMappingController {
-    @Autowired
-    CongMappingRepository repository;
-    
-    @GetMapping()
-    public ResponseEntity<List<CongMapping>> getAll() {
-        try {
-            List<CongMapping> list = new ArrayList<>();
 
-            repository.findAll().forEach(list::add);
-    
-            if(list.isEmpty())
-                    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            
-            return new ResponseEntity<>(list, HttpStatus.OK);
-    
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    private static final Logger log = LoggerFactory.getLogger(CongMappingController.class);
+
+    CongMappingService congMappingService;
+
+    public CongMappingController(CongMappingService congMappingService) {
+        this.congMappingService = congMappingService;
     }
-    
+
+    @GetMapping()
+    public ResponseEntity<List<CongMappingDTO>> getAll() {
+        log.info("received request to list congMapping");
+        return ResponseEntity.ok(congMappingService.getAllCongMapping().asJava());
+    }
+
     @GetMapping("/{id}")
-    public ResponseEntity<CongMapping> getOneById(@PathVariable("id") long id)
+    public ResponseEntity<CongMappingDTO> getOneById(@PathVariable("id") Long id)
     {
-        Optional<CongMapping> data = repository.findById(id);
-        if(data.isPresent())
-            return new ResponseEntity<>(data.get(), HttpStatus.OK);
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        log.info("received request to get one congMapping");
+        return ResponseEntity.ok(congMappingService.getCongMappingById(id));
     }
+
 
     @PostMapping
-    public ResponseEntity<CongMapping> create(@RequestBody CongMapping data) {
-        try {
-            CongMapping created = repository.save(data);
-            return new ResponseEntity<>(created, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
-        }
+    public ResponseEntity<CongMappingDTO> create(@RequestBody CongMappingDTO data) throws URISyntaxException {
+        log.info("received request to create congMapping");
+        CongMappingDTO createdCongMapping = congMappingService.createNew(data);
+        return ResponseEntity.created(new URI("/api/congmapping/" + data.getId())).body(createdCongMapping);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<CongMapping> updateOne(@PathVariable("id") long id, @RequestBody CongMapping data)
+    public ResponseEntity<CongMappingDTO> updateOne(@PathVariable("id") Long id, @RequestBody CongMappingDTO data)
     {
-        Optional<CongMapping> original = repository.findById(id);
-        if(!original.isPresent())
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        data.setId(id);
-        return new ResponseEntity<>(repository.save(data), HttpStatus.OK);
+        log.info("received request to update congMapping");
+        CongMappingDTO congMappingToUpdate = congMappingService.getCongMappingById(id);
+        if (congMappingToUpdate != null) {
+            data.setId(id);
+            congMappingService.update(data);
+        }
+        return ResponseEntity.ok(data);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<HttpStatus> deleteOne(@PathVariable("id") long id)
+    public ResponseEntity<HttpStatus> deleteOne(@PathVariable("id") Long id)
     {
-        try {
-            repository.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
-        }
+        log.info("received request to delete congMapping");
+        congMappingService.delete(id);
+        return ResponseEntity.noContent().build();
     }
-
 }
